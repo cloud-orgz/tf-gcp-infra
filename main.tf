82e659e24d9432dcebd1211245ba7a79ed31a630
Mukul sai Pendem <pendem.mu@northeastern.edu>
2024-02-15 14:12:44 -0500

    modified code to work with .tfvars

--- main.tf
+++ main.tf
@@ -1,32 +1,30 @@
-provider "google" {
-  project = var.project_id
-  region  = var.region
-}
-
-resource "google_compute_network" "vpc" {
-  name                    = "my-vpc"
-  auto_create_subnetworks = false
-  routing_mode            = "REGIONAL"
+resource "google_compute_network" "vpc_network" {
+  name                          = "my-vpc"
+  auto_create_subnetworks       = false
+  routing_mode                  = "REGIONAL"
+  delete_default_routes_on_create = true
 }
 
 resource "google_compute_subnetwork" "webapp_subnet" {
-  name          = "webapp"
-  ip_cidr_range = var.webapp_subnet_cidr
-  region        = var.region
-  network       = google_compute_network.vpc.id
+  name                     = var.webapp_subnet_name
+  ip_cidr_range            = var.webapp_subnet_cidr
+  region                   = var.region
+  network                  = google_compute_network.vpc_network.self_link
+  private_ip_google_access = true
 }
 
 resource "google_compute_subnetwork" "db_subnet" {
-  name          = "db"
-  ip_cidr_range = var.db_subnet_cidr
-  region        = var.region
-  network       = google_compute_network.vpc.id
+  name                     = var.db_subnet_name
+  ip_cidr_range            = var.db_subnet_cidr
+  region                   = var.region
+  network                  = google_compute_network.vpc_network.self_link
+  private_ip_google_access = true
 }
 
-resource "google_compute_route" "webapp_internet_route" {
-  name                    = "webapp-internet-route"
-  dest_range              = "0.0.0.0/0"
-  network                 = google_compute_network.vpc.id
-  next_hop_gateway        = "default-internet-gateway"
-  priority                = 1000
+resource "google_compute_route" "webapp_route" {
+  name             = "${var.webapp_subnet_name}-internet-route"
+  network          = google_compute_network.vpc_network.self_link
+  dest_range       = "0.0.0.0/0"
+  next_hop_gateway = "default-internet-gateway"
 }
+
--- variables.tf
+++ variables.tf
@@ -1,19 +1,33 @@
 variable "project_id" {
-  description = "The project ID in GCP where all resources will be deployed."
-  default     = "cloud-dev-project-414101"
+  description = "The GCP project ID."
+  type        = string
 }
 
 variable "region" {
-  description = "The region where GCP resources will be deployed."
-  default     = "us-east1"
+  description = "The region where resources will be created."
+  type        = string
+}
+
+variable "webapp_subnet_name" {
+  description = "The name of the subnet for the webapp."
+  type        = string
+  default     = "webapp"
+}
+
+variable "db_subnet_name" {
+  description = "The name of the subnet for the database."
+  type        = string
+  default     = "db"
 }
 
 variable "webapp_subnet_cidr" {
-  description = "The CIDR block for the webapp subnet."
+  description = "CIDR block for the webapp subnet."
+  type        = string
   default     = "10.0.1.0/24"
 }
 
 variable "db_subnet_cidr" {
-  description = "The CIDR block for the db subnet."
+  description = "CIDR block for the db subnet."
+  type        = string
   default     = "10.0.2.0/24"
 }
